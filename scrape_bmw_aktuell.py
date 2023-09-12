082aae916f1289fe7779fde48640090ad29300da
Janine Wiesemann <119901184+JeanneDuPre@users.noreply.github.com>
2023-09-12 21:28:39 +0200

    Update scrape_bmw_aktuell.py

--- scrape_bmw_aktuell.py
+++ scrape_bmw_aktuell.py
@@ -8,7 +8,7 @@ def scrape_data(page):
     URL = f"https://www.12gebrauchtwagen.de/suchen?page={page}&utf8=%E2%9C%93&s%5Bsort%5D=6&s%5Bmk%5D=11&s%5Bmd%5D=108&s%5By_min%5D=2016&s%5By_max%5D=&s%5Bm_min%5D=&s%5Bm_max%5D=&s%5Bprice_or_rate%5D=price&s%5Bpr_min%5D=&s%5Bpr_max%5D=&s%5Brate_from%5D=&s%5Brate_to%5D=&s%5Bzip%5D=&s%5Bt%5D=3&s%5Bfuel%5D%5B%5D=2&s%5Bg%5D=m&s%5Bpw_min%5D=74&s%5Bpw_max%5D=110&s%5Bsince%5D=&s%5Bprovider_id%5D%5B%5D=4&s%5Bprovider_id%5D%5B%5D=68&s%5Bprovider_id%5D%5B%5D=26&s%5Bprovider_id%5D%5B%5D=16&s%5Bprovider_id%5D%5B%5D=34&s%5Bprovider_id%5D%5B%5D=35&s%5Bprovider_id%5D%5B%5D=28&s%5Bprovider_id%5D%5B%5D=27&s%5Bprovider_id%5D%5B%5D=2&s%5Bdoors%5D%5B%5D=4-5&s%5Bcu%5D="
 
     response = requests.get(URL, headers={"Accept-Language": "de-DE"})
-    soup = BeautifulSoup(response.content, 'lxml')
+    soup = BeautifulSoup(response.content, 'html.parser')
 
     autos = soup.select('a[class*="car-make-bmw"]')
     
